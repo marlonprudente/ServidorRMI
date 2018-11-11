8f5a6516eb6328bee034f6b7d94d5718becf5cd7
Marlon <marlonoliveira@alunos.utfpr.edu.br>
2018-11-11 20:01:19 -0200

    Inserindo licence header

--- src/main/java/com/marlonprudente/rmi/interfaces/Servidor.java
+++ src/main/java/com/marlonprudente/rmi/interfaces/Servidor.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Este sotfware foi feito para a UTFPR - Campus Curitiba;
+ * O Código é livre para uso não comercial;
+ * Desenvolvido através do Netbeans IDE.
  */
 package com.marlonprudente.rmi.interfaces;
 
